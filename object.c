c5c6b7aede1ad54e798cae2f4c9195e261fc9065
kyle <phantom-kilo@live.com>
2023-03-06 22:14:47 -0700

    Finished part 10, added more attributes. End of night

--- object.c
+++ object.c
@@ -16,60 +16,130 @@ OBJECT objs[] = {
 		 "an open field",
 		tags0,
 		NULL,
-		NULL
+		NULL,
+		NULL,
+		 "The field is a nice and quiet place under a clear blue sky.",
+		"You see",
+		"You really can't get much closer than this.",
+		99,
+		 9999,
+		0
 	},
 	{	/* 1 = cave */
 		 "a little cave",
 		tags1,
 		NULL,
-		NULL
+		NULL,
+		NULL,
+		 "The cave is just a cold, damp, rocky chamber.",
+		"You see",
+		"You really can't get much closer than this.",
+		99,
+		 9999,
+		0
 	},
 	{	/* 2 = silver */
 		 " a silver coin",
 		tags2,
 		 field,
-		NULL
+		NULL,
+		NULL,
+		 "The coin has an eagle on the obverse.",
+		"You see",
+		"You really can't get much closer than this.",
+		 1,
+		0,
+		0
 	},
 	{	/* 3 = gold */
 		 "a gold coin",
 		tags3,
-		 field,
-		NULL
+		 cave,
+		NULL,
+		NULL,
+		 "The shiny coin seems to be a rare and priceless artefact.",
+		"You see",
+		"You really can't get much closer than this.",
+		 1,
+		0,
+		0
 	},
 	{	/* 4 = guard */
 		 "a burly guard",
 		tags4,
 		 field,
-		NULL
+		NULL,
+		NULL,
+		 "The guard is a really big fellow.",
+		 "He has",
+		"You really can't get much closer than this.",
+		99,
+		 20,
+		 100
 	},
 	{	/* 5 = player */
 		 "yourself",
 		tags5,
 		 field,
-		NULL
+		NULL,
+		NULL,
+		 "You need a mirror to look at yourself.",
+		 "You have",
+		"You really can't get much closer than this.",
+		99,
+		 20,
+		 100
 	},
 	{	/* 6 = intoCave */
 		 "a cave entrance to the east",
 		tags6,
 		 field,
-		 cave
+		NULL,
+		 cave,
+		 "The entrance is just a narrow oening in a small outcrop.",
+		"You see",
+		 "The guard stops you from walking in.",
+		99,
+		0,
+		0
 	},
 	{	/* 7 = exitCave */
 		 "an exit to the west",
 		tags7,
 		 cave,
-		 field
+		 field,
+		 field,
+		 "Sunlight pours in through an opening in the cave's wall.",
+		"You see",
+		 "You walk out of the cave.",
+		99,
+		0,
+		0
 	},
 	{	/* 8 = wallField */
 		 "dense forest all around",
 		tags8,
 		 field,
-		NULL
+		NULL,
+		NULL,
+		 "The field is surrounded by trees and undergrowth.",
+		"You see",
+		 "Dense forest is blocking the way.",
+		99,
+		0,
+		0
 	},
 	{	/* 9 = wallCave */
 		 "solid rock all around",
 		tags9,
 		 cave,
-		NULL
+		NULL,
+		NULL,
+		 "Carved in stone is a secret password 'abccb'.",
+		"You see",
+		 "Solid rock is blocking the way.",
+		99,
+		0,
+		0
 	}
 };
