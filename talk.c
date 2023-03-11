85d8f2c1b6a024ebe81936c45f1493e1e0e1d7fe
kyle <phantom-kilo@live.com>
2023-03-10 20:38:53 -0700

    Fixed spelling error

--- talk.c
+++ talk.c
@@ -15,7 +15,7 @@ static void talk(const char *about, OBJECT *to)
     }
     else
     {
-        printf("You here %s say: '%s'\n",
+        printf("You hear %s say: '%s'\n",
                 to->description,
                 topic == to ? "I don't want to talk about myself."
                             : topic->gossip);
